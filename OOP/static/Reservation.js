d70d551a4771ae2e2255dcbaadc8e6cc90b2bc26
Max Faria <mxxfaria@gmail.com>
2023-06-21 22:03:21 -0300

    setters-and-getters

---
+++ OOP/static/Reservation.js
@@ -0,0 +1,32 @@
+class Reservation {
+  constructor(guests, room, days) {
+    this.guests = guests;
+    this.room = room;
+    this.days = days;
+    this.total = Reservation.baseFee * days;
+  }
+
+  static baseFee = 150;
+
+  static showBaseFee() {
+    console.log(`Base fee is ${Reservation.baseFee}`);
+  }
+
+  static get getBaseFee() {
+    return Reservation.baseFee;
+  }
+
+  static get premiumFee() {
+    return Reservation.baseFee * 1.5;
+  }
+}
+
+Reservation.showBaseFee();
+
+const r1 = new Reservation(3, "201", 5);
+console.log(r1);
+
+const 
+
+console.log(`Using gettter: Base fee is ${Reservation.getBaseFee}`);
+console.log(`Premium fee is ${Reservation.premiumFee}`);
