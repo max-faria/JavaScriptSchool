fef86274528fb21989fdabc020ed68d24594255c
Max Faria <mxxfaria@gmail.com>
2023-07-27 06:04:09 -0300

    password-generator-update

--- React/password-generator/src/App.jsx
+++ React/password-generator/src/App.jsx
@@ -1,10 +1,14 @@
 import { useState } from "react";
 import "./App.css";
+import Input from "./componentes/Input";
 
 function App() {
-  const [password, setPassword] = useState("Clique em gerar!");
+  const [password, setPassword] = useState("");
   const [copyText, setCopyText] = useState("Copy");
   const [passwordSize, setPasswordSize] = useState(12);
+  const [showInput, setShowInput] = useState(false);
+
+  const customPassword = showInput ? passwordSize : 8;
 
   function generate() {
     const characters =
@@ -27,17 +31,25 @@ function App() {
     <div className="app">
       <h1>Password Generator</h1>
       <div>
-        <label htmlFor="passwordSize">Length:</label>
+        <label htmlFor="showInput">Customize lenght?</label>
         <input
-          type="number"
-          id="passwordSize"
-          min={1}
-          value={passwordSize}
-          onChange={(ev) => setPasswordSize(ev.target.value)}
+          type="checkbox"
+          id="showInput"
+          value={showInput}
+          onChange={() => setShowInput((currentState) => !currentState)}
         />
       </div>
+      {showInput ? (
+        <div>
+          <label htmlFor="passwordSize">Lenght:</label>
+          <Input
+            passwordSize={passwordSize}
+            setPasswordSize={setPasswordSize}
+          />
+        </div>
+      ) : null}
       <button onClick={generate}>
-        Create a password of {passwordSize} characters!
+        Create a password of {customPassword} characters!
       </button>
       <button onClick={copyToClipboard}>{copyText}</button>
       {/* <button onClick={() => setPasswordSize(99)}>Change Satate</button> */}
---
+++ React/password-generator/src/componentes/Input.jsx
@@ -0,0 +1,13 @@
+import { useState } from "react";
+
+export default function Input(props) {
+  return (
+    <input
+      type="number"
+      id="passwordSize"
+      min={1}
+      value={props.passwordSize}
+      onChange={(ev) => props.setPasswordSize(ev.target.value)}
+    />
+  );
+}
